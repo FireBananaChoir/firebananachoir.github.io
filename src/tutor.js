a85929b12a3f872d635f4a0fe098da15dcf66da0
FireBananaChoir <xjtuhct@126.com>
2018-11-18 13:48:19 +0800

    All is ready

--- src/tutor.js
+++ src/tutor.js
@@ -1,118 +1,123 @@
 var postInfo = [{
-  avatar: '../assets/photos/wzq.png',
+  avatar: '../assets/photos/wzq.jpeg',
   title: '武子骞',
   excerpt: '',
-  link: ''
+  link: 'https://kg3.qq.com/node/play?s=qkW9UVqtdAlWIqED&shareuid=67999481242f328d3c&topsource=a0_pn201001003_z11_u549412568_l1_t1540902919__'
 }, {
-  avatar: '../assets/photos/yzy.png',
+  avatar: '../assets/photos/yzy.jpeg',
   title: '杨智贻',
   excerpt: '',
-  link: ''
+  link: 'https://mp.weixin.qq.com/mp/audio?t=pages/audio_detail&scene=1&__biz=MzI1NDE1MjQ5Nw==&mid=2649372513&idx=5&sn=867a1242988eacb7945bfa329acb68f3&voice_id=MzI1NDE1MjQ5N18yNjQ5MzcyNTEy&_wxindex_=0#wechat_redirect'
 }, {
-  avatar: '../assets/photos/wzx.png',
+  avatar: '../assets/photos/wzx.jpeg',
   title: '韦宗兴',
   excerpt: '',
-  link: ''
+  link: 'https://changba.com/s/7AeYrmQ7oVUpmyXLNisXQg?&code=RkvQSz26klpnyZlkMwLGXMmoND48ll6dnW4ep99i3qn2IbzgPFeMvIthuxuPuiR5o1bwd4kUp9iKq-ue4w8IHLJfx0xqlHGhCCu65c_kvSp9hzfbrId9vHTmj95Ghqzy&code=Gt1bjDM0qnG_X60NZcKtO3UqvztDaccQOwWvroCl6xku3Hbky9giHfRg2HOmtJBdjNqxxPUkDZ2tXxvaFBoyX8ZPZJeAbPzBr7SSIWNt0Eir21RZ2hpnUCKGJFrhHqjP'
 }, {
-  avatar: '../assets/photos/zzp.png',
+  avatar: '../assets/photos/zzp.jpeg',
   title: '周泽平',
   excerpt: '',
-  link: ''
+  link: 'https://changba.com/s/RiILZvBu9D_nWqkP7BHBog?&code=RkvQSz26klqKGZ7yqIdE-ru_P6bdy6OAYUnhw07b4L_mX70vnZ52p06Al2ic9SetBEO50EhwP2YCoed4GvLBjNgCLV_139GJg_7pKF2yZgg0VlwYaM4m0Fn7rqLl4GUIbjn8cvhiJr7JCz7Q0J1Krw&source=wechat#wechat_music_url=7B22736F6E675F5769666955524C223A2268747470733A5C2F5C2F6368616E6762612E636F6D5C2F775C2F5269494C5A76427539445F6E57716B50374248426F673F636F64653D477431626A444D30716E475F5836304E5A634B744F335571767A7444616363516C7370646E4868685237384842364A5653776E504F4C6333736656304F617A6D67734A47634D6E4F73506E62445F47645A6E3030702D4B574551375442554153584E63756A3456426E333433396173694F6C67776F567151616D39715A4267587264656F6B6F5A6167316D41676B4C757A7272456C77222C22736F6E675F5761704C69766555524C223A2268747470733A5C2F5C2F6368616E6762612E636F6D5C2F735C2F5269494C5A76427539445F6E57716B50374248426F673F26636F64653D526B7651537A32366B6C714B475A3779714964452D72755F5036626479364F4159556E6877303762344C5F6D583730766E5A3532703036416C3269633953657442454F3530456877503259436F65643447764C426A4E67434C565F313339474A675F37704B4632795A676730566C7759614D346D30466E3772714C6C34475549626A6E38637668694A72374A437A3751304A314B727726736F757263653D776563686174227D'
 }, {
-  avatar: '../assets/photos/zty.png',
+  avatar: '../assets/photos/zty.jpeg',
   title: '赵天扬',
   excerpt: '',
-  link: ''
+  link: 'https://kg2.qq.com/node/play?s=mXLgL-mA-lR5pmFU&shareuid=659a9d802628368835&topsource=a0_pn201001003_z11_u770535131_l1_t1540904207__'
 }, {
-  avatar: '../assets/photos/dyl.png',
+  avatar: '../assets/photos/dyl.jpeg',
   title: '邓雅琳',
   excerpt: '',
-  link: ''
+  link: 'https://changba.com/s/hPB8IAJ7hHFhTuqs-aUqrg?&code=RkvQSz26klp_bkTJ8eIp_G2GHnkVV92amEd8E8sa8kkireC0tUhOWrwuNDyX1mpO5kuqWjTG4MaU0LtqgIkOEOzQ0rfodsY-iDCM9m63yEqaTL_RQPUUVJgV-MF_ExUdlJVSqecmb0RcAQLkjPn3WA&source=wechat#wechat_music_url=7B22736F6E675F5769666955524C223A2268747470733A5C2F5C2F6368616E6762612E636F6D5C2F775C2F6850423849414A3768484668547571732D61557172673F636F64653D477431626A444D30716E475F5836304E5A634B744F79356932673474375F752D496D537567693761483738366C62674233506C4671325A4B6E574B3353714F39496A4A70433076726631515F4D526C34596C574937757352784562754F5559687168344F4970777A724B34783643696C766234784E715F6470444A5F61317A6A66305933477230516C3751222C22736F6E675F5761704C69766555524C223A2268747470733A5C2F5C2F6368616E6762612E636F6D5C2F735C2F6850423849414A3768484668547571732D61557172673F26636F64653D526B7651537A32366B6C705F626B544A386549705F473247486E6B56563932616D45643845387361386B6B69726543307455684F577277754E447958316D704F356B7571576A5447344D6155304C747167496B4F454F7A513072666F6473592D6944434D396D363379457161544C5F5251505555564A67562D4D465F457855646C4A56537165636D6230526341514C6B6A506E33574126736F757263653D776563686174227D'
 }, {
-  avatar: '../assets/photos/djh.png',
+  avatar: '../assets/photos/djh.jpeg',
   title: '董锦红',
   excerpt: '',
-  link: ''
+  link: 'https://kg.qq.com/node/play?s=_g0-L9_Kzb5Zn_Me&shareuid=639c9c842c2b3e8b32&topsource='
 }, {
-  avatar: '../assets/photos/axy.png',
+  avatar: '../assets/photos/axy.jpeg',
   title: '阿雪莹',
   excerpt: '',
-  link: ''
+  link: 'https://kg.qq.com/node/play?s=o-8NgMox2mWwGoZf&shareuid=639c9f8c202f378b3c&topsource=a0_pn201001003_z11_u112952008_l1_t1540865501__'
 }, {
-  avatar: '../assets/photos/xjr.png',
+  avatar: '../assets/photos/xjr.jpeg',
   title: '徐佳荣',
   excerpt: '',
-  link: ''
+  link: 'https://changba.com/s/Pzl_dKQVjkQ928hnAUYgWQ?&code=RkvQSz26klqSvIHqydvljRNDdVUiEfJJjtcHrYNL5ZDIHaeUyNKDZPnRQohalMXgWOBSxqmwXYr8Vgo7gB-Xd6BHNL_pmwKnrKySS5A4GauyxnULbmuJIpwdO-UG1e_f&code=Gt1bjDM0qnG_X60NZcKtO3UqvztDaccQeUvk7MlovNt9DWdXS4JfPBncW5UQ3T0VCG5z8Qfnt1vdUXzzqlcYemUHhKqFZeGDa5UrPZuANWqb-gh9EMalqj3ABSvzct6p'
 }, {
-  avatar: '../assets/photos/qfd.png',
+  avatar: '../assets/photos/qfd.jpeg',
   title: '卿锋镝',
   excerpt: '',
-  link: ''
+  link: 'https://changba.com/s/SplN1Uq5ynnnzs24O8RB9w?&code=RkvQSz26klomBsZ7C-vvFC6MwCcS4UStlZWutZhK2SbvG1Edjx_A7u15Ot8BixhQQ75-KYeR13C_HQyiP5JrzIJaBgn9YiAehueEWrMs7UUjkuUcB06RSqCDUna-tPI6&code=Gt1bjDM0qnG_X60NZcKtO3UqvztDaccQkA0Bauu6X4zjJvaXFUTn87qRa94YxZlOhUfyiKHCnAYMjp_C15RDnbTmpIW1WbUaqUa_VqMm2Keeszhn_mI7aGulJoPR3xG9'
 }, {
-  avatar: '../assets/photos/zxj.png',
+  avatar: '../assets/photos/zxj.jpeg',
   title: '张歆婕',
   excerpt: '',
-  link: ''
+  link: 'https://node.kg.qq.com/play?s=tEbRhKtqM9Wogt15&shareuid=669e9f8c2c2b3082&topsource='
 }, {
-  avatar: '../assets/photos/hyr.png',
+  avatar: '../assets/photos/hyr.jpeg',
   title: '黄怡然',
   excerpt: '',
-  link: ''
+  link: 'https://kg3.qq.com/node/play?s=tT3Y-5tqdlVS3tQ1&shareuid=609594802d25328c37&topsource=a0_pn201001003_z11_u289588573_l0_t1541749689__'
 }, {
-  avatar: '../assets/photos/lxq.png',
+  avatar: '../assets/photos/qy.jpeg',
+  title: '丘延',
+  excerpt: '',
+  link: 'https://changba.com/s/Mhr5L1VbyAIqGV9v6Q9b_Q?&code=RkvQSz26klrJchIHWAA6Wcq3uOQLs9KTa_cie-6dy_y9HKewwZXkzQimgnCHp8lNC0JbBoTau4CCk18gf3K4ZV-Sffv_l9BTRmBzG8ndPtiS4woE5TgFItkUjigiYN3X5V_rMJqgoCHdpiRkq_JslA&source=wechat#wechat_music_url=7B22736F6E675F5769666955524C223A2268747470733A5C2F5C2F6368616E6762612E636F6D5C2F775C2F4D6872354C3156627941497147563976365139625F513F636F64653D477431626A444D30716E475F5836304E5A634B744F5F583037714B64683034764A635076527438716244704B7A75456C784F4A782D4A56737246504E6562533344646C2D427756783838776C5F6F364A7A6F31654A416F6B4E496B624F6A3479596A5968595233476B595F36425A584775636E712D6A673079367179685651546A49755A56767335725855644F576E49314E34556277222C22736F6E675F5761704C69766555524C223A2268747470733A5C2F5C2F6368616E6762612E636F6D5C2F735C2F4D6872354C3156627941497147563976365139625F513F26636F64653D526B7651537A32366B6C724A636849485741413657637133754F514C73394B54615F6369652D3664795F7939484B6577775A586B7A51696D676E434870386C4E43304A62426F5461753443436B31386766334B345A562D536666765F6C394254526D427A47386E645074695334776F453554674649746B556A696769594E335835565F724D4A71676F4348647069526B715F4A736C4126736F757263653D776563686174227D'
+}, {
+  avatar: '../assets/photos/lxq.jpeg',
   title: '李星奇',
   excerpt: '',
-  link: ''
+  link: 'https://kg.qq.com/node/play?s=7u_uOU7ZFybfn7-V&shareuid=669594852425338a37&topsource=a0_pn201001003_z11_u489018413_l1_t1540828866__'
 }, {
-  avatar: '../assets/photos/crq.png',
+  avatar: '../assets/photos/crq.jpeg',
   title: '常芮荃',
   excerpt: '',
-  link: ''
+  link: 'https://node.kg.qq.com/play?s=2G-OpO2Qxs3mP2AK&shareuid=609f9880252934823c&topsource=a0_pn201001003_z11_u225504398_l0_t1541671420__'
 }, {
-  avatar: '../assets/photos/lhn.png',
+  avatar: '../assets/photos/lhn.jpeg',
   title: '刘浩男',
   excerpt: '',
-  link: ''
+  link: 'https://mp.weixin.qq.com/mp/audio?t=pages/audio_detail&scene=1&__biz=MzI1NDE1MjQ5Nw==&mid=2649372504&idx=5&sn=45b54bc1d39d37e7fb6cadbc54a6d7af&voice_id=MzI1NDE1MjQ5N18yNjQ5MzcyNTAx&_wxindex_=0#wechat_redirect'
 }, {
-  avatar: '../assets/photos/bys.png',
+  avatar: '../assets/photos/bys.jpeg',
   title: '保伊莎',
   excerpt: '',
-  link: ''
+  link: 'https://kg2.qq.com/node/play?s=oHbsjboxQb3h5odS&shareuid=609b9b85252f358834&topsource=a0_pn201001003_z11_u266002230_l0_t1540896646__'
 }, {
-  avatar: '../assets/photos/wty.png',
+  avatar: '../assets/photos/wty.jpeg',
   title: '王天一',
   excerpt: '',
-  link: ''
+  link: 'https://kg3.qq.com/node/play?s=enQTCYevP1S7gepQ&shareuid=669e9885262a318d37&topsource=a0_pn201001003_z11_u435037663_l0_t1540819374__'
 }]
 
 function createPostCard(imageUrl, title, excerpt, pageLink) {
   var cardArticle = document.createElement('article')
   var imageLinkA = document.createElement('a')
   var imageDiv = document.createElement('div')
-  // var contentLinkA = document.createElement('a')
-  // var contentDiv = document.createElement('div')
-  // var titleH2 = document.createElement('h2')
-  // var excerptSection = document.createElement('section')
-  // var excerptP = document.createElement('p')
+  var contentLinkA = document.createElement('a')
+  var contentDiv = document.createElement('div')
+  var titleH2 = document.createElement('h2')
+  var excerptSection = document.createElement('section')
+  var excerptP = document.createElement('p')
   cardArticle.classList.add('post-card')
   imageLinkA.classList.add('post-card-image-link')
   imageLinkA.href = pageLink
   imageDiv.classList.add('post-card-image')
   imageDiv.style.backgroundImage = `url(${imageUrl})`
-  // contentLinkA.classList.add('post-card-content-link')
-  // contentLinkA.href = pageLink
-  // contentDiv.classList.add('post-card-content')
-  // titleH2.classList.add('post-card-title')
-  // titleH2.innerHTML = title
-  // excerptSection.classList.add('post-card-excerpt')
-  // excerptP.innerHTML = excerpt
+  contentLinkA.classList.add('post-card-content-link')
+  contentLinkA.href = pageLink
+  contentDiv.classList.add('post-card-content')
+  titleH2.classList.add('post-card-title')
+  titleH2.innerHTML = title
+  excerptSection.classList.add('post-card-excerpt')
+  excerptP.innerHTML = excerpt
   cardArticle.appendChild(imageLinkA)
-  // cardArticle.appendChild(contentLinkA)
+  cardArticle.appendChild(contentLinkA)
   imageLinkA.appendChild(imageDiv)
-  // contentLinkA.appendChild(contentDiv)
-  // contentDiv.appendChild(titleH2)
-  // contentDiv.appendChild(excerptSection)
-  // excerptSection.appendChild(excerptP)
+  contentLinkA.appendChild(contentDiv)
+  contentDiv.appendChild(titleH2)
+  contentDiv.appendChild(excerptSection)
+  excerptSection.appendChild(excerptP)
   return cardArticle
 }
 
